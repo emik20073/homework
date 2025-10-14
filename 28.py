309b376ccf0116dbe54ad3d59d2fa2f3f49f7e9c
emik20073 <emiltrofimenko2007@gmail.com>
2025-10-14 19:12:12 +0300

    commit

---
+++ 28.py
@@ -0,0 +1,17 @@
+def generate_cube_numbers(end):
+    n = 2
+    while True:
+        cube = n ** 3
+        if cube > end:
+            break
+        yield cube
+        n += 1
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8]
+assert list(generate_cube_numbers(100)) == [8, 27, 64]
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000]
+print('Ok')
